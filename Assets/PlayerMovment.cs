87f0d68d7e4bfb7fa9850a365dd63612ed62ce1d
Viktor Stoyanov <vikman.enterprise@gmail.com>
2019-02-20 15:46:21 +0100

    Player can control the fox, all movment maanaged, animations managed

---
+++ Assets/PlayerMovment.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public Animator animator;
+
+
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+
+
+    bool jump = false;
+    bool crouch = false;
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+
+        // Conditional for jumping
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("isJumping", true);
+        }
+
+        // Conditionnal for crouching
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    // Managing the state of isJumping when the Player touch the ground 
+    public void OnLanding()
+    {
+        animator.SetBool("isJumping", false);
+    }
+
+    // managing the state of the crouch
+    public void OnCrouching(bool isCrouching)
+    {
+        animator.SetBool("IsCrouching", isCrouching);
+    }
+
+    // Setting the movment
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch , jump);
+        jump = false;
+
+    }
+}
